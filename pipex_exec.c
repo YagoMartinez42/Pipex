9a54ea620dd50d7b11c4dbe9275b566c31526cd1
Santiago Martinez Fernandez <samartin@c1r14s1.42madrid.com>
2024-05-28 16:51:26 +0200

    wip

--- pipex_exec.c
+++ pipex_exec.c
@@ -6,7 +6,7 @@
 /*   By: samartin <samartin@student.42madrid.es>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 14:30:28 by samartin          #+#    #+#             */
-/*   Updated: 2024/05/21 16:59:40 by samartin         ###   ########.fr       */
+/*   Updated: 2024/05/28 16:50:07 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	pp_pipex_files(int argc, char **argv, char **envp)
 	int		pid;
 	t_list	*pipe_lst;
 
-	pipe_lst = pp_create_pipes(argc - 2);
+	pipe_lst = pp_create_pipes(argc - 2); //NONO!! Argc tiene 3 o 4 que no son commands
 	if (!pipe_lst)
 	{
 		perror("Memory error creating pipes.\n");
@@ -64,8 +64,14 @@ int	pp_pipex_files(int argc, char **argv, char **envp)
 		}
 		else if (pid)
 		{
-			//close all all pipes except mines
-			//execve
+			if (i == 1)
+				//open infile and dup2 to stdin
+			else if (i == argc - 2)
+			{
+				//open outfile and dup2 to stdout
+			}
+			//close all pipes except mines
+			//execve my argv
 		}
 	}
 	waitpid(pid);
